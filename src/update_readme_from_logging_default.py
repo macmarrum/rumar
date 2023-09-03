191def66d064ec91c869bb5f906753792713e847
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-03 23:33:26 +0200

    add update_readme_from_logging_default.py

---
+++ src/update_readme_from_logging_default.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from rumar import LOGGING_TOML_DEFAULT
+
+me = Path(__file__)
+UTF8 = 'UTF-8'
+
+readme_md_path = me.parent.parent / 'README.md'
+lines = []
+is_settings = False
+for line in readme_md_path.read_text(encoding=UTF8).splitlines():
+    if line.startswith('<!-- logging settings begin -->'):
+        is_settings = True
+        lines.append(line)
+    if line.startswith('<!-- logging settings end -->'):
+        is_settings = False
+        lines.append('```toml')
+        lines += LOGGING_TOML_DEFAULT.splitlines()
+        lines.append('```')
+    if not is_settings:
+        lines.append(line)
+text = '\n'.join(lines) + '\n'
+readme_md_path.rename(readme_md_path.with_suffix('.bak'))
+readme_md_path.write_text(text, encoding=UTF8)
