85df5a990d4e6969c02566ece61eff7254aec5bd
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-10-11 15:59:03 +0200

    add test_can_match_file__no_inc__exc_full_star_single_midlevel_dir

--- tests/test_rumar_core.py
+++ tests/test_rumar_core.py
@@ -431,6 +431,42 @@ class TestRumarCore:
         }
         assert actual == expected
 
+    def test_can_match_file__no_inc__exc_full_star_single_midlevel_dir(self, set_up_rumar):
+        d = set_up_rumar
+        profile = d['profile']
+        profile_to_settings = d['profile_to_settings']
+        settings = profile_to_settings[profile]
+        settings = replace(settings,  # NOTE: local settings dict, not in rumar
+                           excluded_files=['A/*'],
+                           )
+        rumar = d['rumar']
+        R = lambda p: Rather(f"{profile}/{p}", lstat_cache=rumar.lstat_cache)
+        expected = {
+            'file01.txt': 1,
+            'file02.txt': 1,
+            'file03.csv': 1,
+            'A/file04.txt': 0,
+            'A/file05.txt': 0,
+            'A/file06.csv': 0,
+            'A/A-A/file13.txt': 1,
+            'A/A-A/file14.txt': 1,
+            'A/A-A/file15.csv': 1,
+            'A/A-B/file16.txt': 1,
+            'A/A-B/file17.txt': 1,
+            'A/A-B/file18.csv': 1,
+            'B/file07.txt': 1,
+            'B/file08.txt': 1,
+            'B/file09.csv': 1,
+            'AA/file10.txt': 1,
+            'AA/file11.txt': 1,
+            'AA/file12.csv': 1,
+        }
+        actual = {
+            psx: _can_match_file(r := R(psx), settings, derive_relative_psx(r, r.BASE_PATH, with_leading_slash=True))
+            for psx in expected.keys()
+        }
+        assert actual == expected
+
     def test_can_match_file__inc_full_single_dir__exc_full_seq_in_same_dir__inc_dir_rx_single__inc_top_dir_single__inc_glob__exc_glob(self, set_up_rumar):
         d = set_up_rumar
         profile = d['profile']
