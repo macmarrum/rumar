9fa9434786d4d201d9243260bfa621a92a6e8404
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-11-28 09:29:35 +0100

    Settings.__str__: change order

--- src/rumar.py
+++ src/rumar.py
@@ -588,8 +588,8 @@ class Settings:
     def __str__(self):
         return ('{'
                 f"profile: {self.profile!r}, "
-                f"backup_dir: {self.backup_dir.__str__()!r}, "
-                f"source_dir: {self.source_dir.__str__()!r}"
+                f"source_dir: {self.source_dir.__str__()!r} ,"
+                f"backup_dir: {self.backup_dir.__str__()!r}"
                 '}')
 
     def print(self):
